4a5598b1c4694b5639d6e8d34b3f70d2f2ae1ae7
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-06-06 21:48:44 +0200

    init

---
+++ src/main/java/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,28 @@
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+
+	public static void main(String[] args) {
+		
+		
+		String[] invitati = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("inserisci il tuo nome");
+		String nome = scanner.nextLine() ;
+		scanner.close();
+		int i=0;
+		while (i < invitati.length) {
+			System.out.println("l'indice numero: "+ i + " nome: "+nome + " confronto: "+invitati[i]);
+			if (invitati[i] != nome) {
+				System.out.println("puoi entrare");
+			} else {
+				System.out.println("non puoi entrare");
+			}
+			i++;
+		}
+
+	}
+
+}
