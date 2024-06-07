5e8c28054bcfdcaa4c429e576607cca50a8e504e
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-06-07 19:02:45 +0200

    fix

--- src/main/java/com/ferragnez/party/CheckGuest.java
+++ src/main/java/com/ferragnez/party/CheckGuest.java
@@ -6,11 +6,11 @@ public class CheckGuest {
 
 	public static void main(String[] args) {
 		
-		
+		String nome;
 		String[] invitati = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("inserisci il tuo nome");
-		String nome = scanner.nextLine() ;
+		nome = scanner.nextLine() ;
 		scanner.close();
 		int i=0;
 		while (i < invitati.length) {
